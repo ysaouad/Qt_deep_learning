193ca6fabc89a94e2e914924308f3769e9fc9dbc
ysaouad <97282242+ysaouad@users.noreply.github.com>
2022-08-10 16:31:11 +0200

    double deep q learning
    almost the same as regular deep q learning, but this time, the action value evaluation is done with a different network than the action value estimate.

---
+++ atari_double_deep_q_learning/DQN.py
@@ -0,0 +1,50 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch as T
+import os
+import time
+
+
+class LinearDeepQNetwork(nn.Module):
+    def __init__(self, lr, n_actions, input_size):
+        super(LinearDeepQNetwork, self).__init__()
+        self.cv1 = nn.Conv2d(input_size[0], 32, 8, stride=4)
+        self.cv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.cv3 = nn.Conv2d(64, 64, 3, stride=1)   
+        input_size_fc1 = self.output_size_cv3(input_size)
+
+        self.fc1 = nn.Linear(input_size_fc1, 512)
+        self.fc2 = nn.Linear(512, n_actions)
+
+        self.optimizer = T.optim.RMSprop(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cuda' if T.cuda.is_available() else 'cpu')
+        self.to(self.device) 
+        
+    def forward(self, state):
+        cv1 = F.relu(self.cv1(state))
+        cv2 = F.relu(self.cv2(cv1))
+        cv3 = F.relu(self.cv3(cv2))
+        cv3 = cv3.view(cv3.size()[0],-1)
+        fc1 = F.relu(self.fc1(cv3))
+        actions = self.fc2(fc1)
+        return actions
+    
+    def output_size_cv3(self, input_size):
+        shape = T.zeros(1, *input_size)
+        shape = self.cv1(shape)
+        shape = self.cv2(shape)
+        shape = int(np.prod(self.cv3(shape).size()))
+
+        return shape
+    
+    def save_checkpoint(self):
+        print('... saving checkpoint ...')
+        T.save(self.state_dict(), r'C:\Users\YAA5\Documents\DQN')
+        
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.load_state_dict(T.load(r'C:\Users\YAA5\Documents\DQN'))
---
+++ atari_double_deep_q_learning/agent.py
@@ -0,0 +1,106 @@
+import torch as T
+import os
+from DQN import LinearDeepQNetwork
+import numpy as np
+
+class Agent():
+    def __init__(self, input_dims, n_actions, lr, mem_size, batch_size, gamma=0.99,
+                 epsilon=1.0, eps_dec=1e-5, eps_min=0.1):
+        self.input_dims = input_dims
+        self.n_actions = n_actions
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_dec = eps_dec
+        self.eps_min = eps_min
+        self.action_space = [i for i in range(n_actions)]
+        self.batch_size = batch_size
+        self.lr = lr
+        self.Q = LinearDeepQNetwork(self.lr, self.n_actions, self.input_dims)
+        self.Q_ = LinearDeepQNetwork(self.lr, self.n_actions, self.input_dims)
+        self.step = 0
+        self.mem_size = mem_size
+        self.index_mem = 0
+        
+        self.states = np.zeros((self.mem_size, *input_dims),dtype=np.float32)
+        self.states_ = np.zeros((self.mem_size, *input_dims),dtype=np.float32)
+        self.actions= np.zeros(self.mem_size, dtype=np.int64)
+        self.rewards = np.zeros(self.mem_size, dtype=np.float32)
+        self.dones = np.zeros(self.mem_size, dtype=np.bool)
+        
+    def choose_action(self, observation):
+        rand = np.random.random()
+        if np.random.random() > self.epsilon:
+            if rand > 0.5:
+                state = T.tensor(np.array([observation]), dtype=T.float).to(self.Q.device)
+                actions = self.Q.forward(state)
+            else:
+                state = T.tensor(np.array([observation]), dtype=T.float).to(self.Q.device)
+                actions = self.Q.forward(state)
+            action = T.argmax(actions).item()
+        else:
+            action = np.random.choice(self.action_space)
+        return action
+
+    def exp_replay(self, state, action, reward, state_, done):
+            self.index_mem = self.step % self.mem_size
+            
+            self.step +=1
+            self.states[self.index_mem] = state
+            self.states_[self.index_mem] = state_
+            self.actions[self.index_mem] = action
+            self.rewards[self.index_mem] = reward
+            self.dones[self.index_mem] = done
+            
+            if self.batch_size > self.step:
+             return 
+            
+            curr_mem = min(self.mem_size, self.step)
+            
+            index_batch = np.random.choice(curr_mem, self.batch_size, replace=False)
+            states = self.states[index_batch]
+            states_ = self.states_[index_batch]
+            actions = self.actions[index_batch]
+            rewards = self.rewards[index_batch]
+            dones = self.dones[index_batch]
+            
+
+            self.learn(states, states_, actions, rewards, dones)
+            
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.eps_dec \
+                        if self.epsilon > self.eps_min else self.eps_min
+
+    def learn(self, states, states_, actions, rewards, dones):
+       
+        self.Q.optimizer.zero_grad()
+        
+        states = T.tensor(states).to(self.Q.device)
+        actions = T.tensor(actions).to(self.Q.device)
+        rewards = T.tensor(rewards).to(self.Q.device)
+        states_ = T.tensor(states_).to(self.Q.device)
+        dones = T.tensor(dones).to(self.Q.device)
+        indices = np.arange(self.batch_size)
+        
+        q_pred = self.Q.forward(states)[indices,actions]
+        q_next = self.Q_.forward(states_)
+        Q = self.Q.forward(states_)
+        
+        argmax = T.argmax(Q, dim=1)
+        
+        q_next[dones] = 0.0
+        q_target = rewards + self.gamma*q_next[indices, argmax]
+
+        loss = self.Q.loss(q_target, q_pred).to(self.Q.device)
+        
+        loss.backward()
+        self.Q.optimizer.step()
+  
+        self.decrement_epsilon()
+        
+    def save_models(self):
+        self.Q.save_checkpoint()
+        self.Q_.save_checkpoint()
+
+    def load_models(self):
+        self.Q_.load_checkpoint()
+        self.Q.load_checkpoint()
